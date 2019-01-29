7297776d5c44f780ec8d7af2fdd33666bc2c61cc
James <James@DESKTOP-9DPKHEF>
2019-01-29 12:29:06 +0000

    Add project files.

---
+++ FACodeTest.RobotWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FACodeTest.RobotWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
